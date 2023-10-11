d49427fbfac8a7d42f4226f6a6d82ad21edbd69b
HyunJun Baek <hjbaek5796@gmail.com>
2023-10-11 22:04:47 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:game_app_clone/screens/game_app_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Game App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const GameAppScreen(),
+    );
+  }
+}
---
+++ lib/screens/game_app_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GameAppScreen extends StatefulWidget {
+  const GameAppScreen({super.key});
+
+  @override
+  State<GameAppScreen> createState() => _GameAppScreenState();
+}
+
+class _GameAppScreenState extends State<GameAppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
