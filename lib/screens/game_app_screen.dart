7c6cc8a92c2b063412becf2dea1b3411d4a29afa
HyunJun Baek <hjbaek5796@gmail.com>
2023-10-14 18:43:26 +0900

    installed flutter_animate + implemented page_controller

--- lib/screens/game_app_screen.dart
+++ lib/screens/game_app_screen.dart
@@ -8,17 +8,24 @@ class GameAppScreen extends StatefulWidget {
 }
 
 class _GameAppScreenState extends State<GameAppScreen> {
+  final PageController _pageController = PageController(
+    viewportFraction: 0.6,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           PageView.builder(
+            controller: _pageController,
             itemCount: 5,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
               return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Container(),
                   Container(
                     height: 350,
                     decoration: BoxDecoration(
